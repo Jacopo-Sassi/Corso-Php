b94a78799ff7ce2163e20bf6c73ae26e1cf34591
Jacopo-Sassi <sassijacopp@gmail.com>
2025-02-17 11:34:33 +0100

    esercizio 3

---
+++ php-snack-1/esercizio-3/index.php
@@ -0,0 +1,9 @@
+<?php
+$variabileUno = 20;
+$variabileDue = 25;
+echo $variabileUno + $variabileDue;
+echo "<br>";
+$variabileTre = 10;
+$variabileQuattro = 5;
+echo $variabileTre * $variabileQuattro;
+?>
