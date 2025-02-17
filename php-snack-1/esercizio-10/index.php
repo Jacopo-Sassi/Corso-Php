acd0af9eae85727a3a0e6fd2e3398f26ce7e8884
Jacopo-Sassi <sassijacopp@gmail.com>
2025-02-17 12:11:52 +0100

    Esercizi 9 e 10 terminati

---
+++ php-snack-1/esercizio-10/index.php
@@ -0,0 +1,8 @@
+<?php
+$orario = 11;
+if ($orario < 12) {
+    echo "<img src='https://imgs.search.brave.com/4O94w-ceA0jeEsleJUevPQiwEgIahOS-o0tSiBIW_g8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWNnZW9wb3AuYWth/bWFpemVkLm5ldC93/cC1jb250ZW50L3Vw/bG9hZHMvc2l0ZXMv/MzIvMjAyMi8wNi9B/Y3Rpdml0eV9Db250/aW51ZXNfT25fdGhl/X1N1bi5qcGc_aW09/UmVzaXplLHdpZHRo/PTYzODs'>";
+} elseif ($orario > 12) {
+    echo "<img src='https://imgs.search.brave.com/iGV-ljseqj_2JQOfKMk-I3fU33FHSsqKmkGt8vDwnXg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtYXNzZXRzLm5h/c2EuZ292L2ltYWdl/L1BJQTAwNDA0L1BJ/QTAwNDA0fmxhcmdl/LmpwZz93PTE5MjAm/aD0xOTIwJmZpdD1j/bGlwJmNyb3A9ZmFj/ZXMsZm9jYWxwb2lu/dA'>";
+}
+?>
---
+++ php-snack-1/esercizio-9/index.php
@@ -0,0 +1,9 @@
+<?php
+$isOnline = false;
+if ($isOnline) {
+    echo "<p style='color: green'>L'utente è online</p>";
+}
+else {
+    echo "<p style='color: red'>L'utente è offline</p>";
+}
+?>
