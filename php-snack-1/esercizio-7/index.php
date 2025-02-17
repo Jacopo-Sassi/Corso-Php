cedba22ab4f6ea7c9c72cd1b58af9affc3010127
Jacopo-Sassi <sassijacopp@gmail.com>
2025-02-17 11:50:12 +0100

    Completati esercizi 7 e 8

---
+++ php-snack-1/esercizio-7/index.php
@@ -0,0 +1,19 @@
+<?php
+$titolo = "La Grande Migrazione dei Goti";
+$sottotitolo = "La storia di un popolo";
+$descrizione = "I goti erano una popolazione estremamente variegata e complessa, che comprendeva diversi gruppi etnici e culturali. La loro migrazione fu un evento epocale che cambiò per sempre il volto dell'Europa.";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercizio-5</title>
+</head>
+<body>
+    <h1><?php echo $titolo; ?></h1>
+    <h2><?php echo $sottotitolo; ?></h2>
+    <p><?php echo $descrizione; ?></p>
+</body>
+</html>
---
+++ php-snack-1/esercizio-8/index.php
@@ -0,0 +1,8 @@
+<?php
+$eta = 17;
+if($eta >=18)
+    {echo "Sei maggiorenne";}
+else {
+    echo "Sei minorenne";
+}
+?>
