c8326022c1b23560218ddb27d6d9051cf9e502a6
Jacopo-Sassi <sassijacopp@gmail.com>
2025-02-17 11:25:40 +0100

    Esercizio 1 completato

--- php-snack-1/esercizio-1/index.php
+++ php-snack-1/esercizio-1/index.php
@@ -1,16 +1,3 @@
 <?php
-$saluto = "Ciao";
-echo $saluto;
+echo "Benvenuto al corso di PHP!";
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
