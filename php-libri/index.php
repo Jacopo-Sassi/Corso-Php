90f5aefb7aec17cf0905b4af7ec344fc3a8e6508
Jacopo-Sassi <sassijacopp@gmail.com>
2025-03-21 16:45:38 +0100

    Aggiunta la possibilità di inserire un nuovo libro + protezione sql injection

--- php-libri/index.php
+++ php-libri/index.php
@@ -27,6 +27,37 @@ if (isset($_POST['genere']) && !empty($_POST['genere'])) {
 
     $result = mysqli_stmt_get_result($stmt);
     }
+
+    if (isset($_POST['titolo']) && isset($_POST['autore']) && isset($_POST['add-genere']) 
+    && isset($_POST['anno']) && isset($_POST['prezzo']) && isset($_POST['isbn'])) {
+    
+    $titolo = $_POST['titolo'];
+    $autore = $_POST['autore'];
+    $addGenere = $_POST['add-genere'];
+    $anno = $_POST['anno'];
+    $prezzo = $_POST['prezzo'];
+    $isbn = $_POST['isbn'];
+
+    if (!empty($titolo) && !empty($autore) && !empty($addGenere) 
+        && !empty($anno) && !empty($prezzo) && !empty($isbn)) {
+        
+        $query2 = "INSERT INTO libri (titolo, autore, genere, anno_pubblicazione, prezzo, isbn) 
+                   VALUES (?, ?, ?, ?, ?, ?)";
+        $stmt2 = mysqli_prepare($conn, $query2);
+        
+        mysqli_stmt_bind_param($stmt2, "sssids", $titolo, $autore, $addGenere, $anno, $prezzo, $isbn);
+
+        if (mysqli_stmt_execute($stmt2)) {
+            echo "Inserimento avvenuto con successo";
+        } else {
+            echo "Errore nell'inserimento: " . mysqli_stmt_error($stmt2);
+        }
+
+        mysqli_stmt_close($stmt2);
+    } else {
+        echo "Tutti i campi devono essere compilati!";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -42,7 +73,20 @@ if (isset($_POST['genere']) && !empty($_POST['genere'])) {
 <form action="index.php" method="POST">
     <label for="genere">Genere:</label>
     <input type="text" id="genere" name="genere">
-    <button type="submit">Cerca</button>
+    <button type="submit">Cerca</button> 
+    <label for ="titolo">Titolo:</label>
+    <input type = "text" id="titolo" name ="titolo"> 
+    <label for ="autore">Autore:</label>
+    <input type = "text" id="autore" name ="autore"> 
+    <label for ="add-genere">Genere:</label>
+    <input type = "text" id="add-genere" name ="add-genere">
+    <label for ="anno">Anno:</label>
+    <input type = "text" id="anno" name ="anno">
+    <label for ="prezzo">Prezzo:</label>
+    <input type = "text" id="prezzo" name ="prezzo">
+    <label for ="isbn">ISBN:</label>
+    <input type = "text" id="isbn" name ="isbn">
+    <button type="submit">Inserisci</button>
 </form>
 
 
