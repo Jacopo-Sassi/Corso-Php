96580eb61d7444b5e5507098ba4a71c81dac9e99
Jacopo-Sassi <sassijacopp@gmail.com>
2025-03-28 16:48:30 +0100

    completata la funzione di registrazione

--- login/server/register.php
+++ login/server/register.php
@@ -11,6 +11,19 @@
     if (isset($_POST['username']) && isset($_POST['password'])) {
         $username = $_POST['username'];
         $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-        echo $username . "/" . $password;
+
+        $query = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $stmt = mysqli_prepare($conn, $query);
+        mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+        mysqli_stmt_execute($stmt);
+
+        if (mysqli_stmt_affected_rows($stmt) > 0) {
+
+            $msg = "Registrazione avvenuta con successo!";
+        } else {
+            $msg ="Errore nella registrazione: " . mysqli_error($conn);
+        }
+        echo $msg;
     }
+    
 ?>
